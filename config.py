f59e4e931c700d658e5d74e172622fc7273d051f
JeaDong <704404536@qq.com>
2018-04-10 15:20:42 +0800

    connect mysql

--- config.py
+++ config.py
@@ -10,3 +10,5 @@ class DevConfig(Config):
     """Development config class."""
     # Open the DEBUG
     DEBUG = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@127.0.0.1:3306/myblog'
